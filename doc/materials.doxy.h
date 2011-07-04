d96c353a988f60495ec2db3f5e81a8d4fbf06556
Soulisse Baptiste <soulisse@polytech.unice.fr>
2011-07-04 10:58:48 +0200

    Add a module which defines some materials.

---
+++ doc/materials.doxy.h
@@ -0,0 +1,15 @@
+/**
+ * @defgroup Materials Materials
+ * @ingroup Modules
+ *
+ * Define some useful materials.
+ *
+ * This module allows you to add some useful materials in your different objects.
+ *
+ *
+ * @{
+ */
+
+/**
+* @}
+*/
