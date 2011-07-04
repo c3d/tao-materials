c04e3e805ec28bfe11952ddcfc5e8233c54a0840
Soulisse Baptiste <soulisse@polytech.unice.fr>
2011-07-04 11:54:38 +0200

    Add documentation for Materials module.

--- doc/materials.doxy.h
+++ doc/materials.doxy.h
@@ -4,11 +4,175 @@
  *
  * Define some useful materials.
  *
- * This module allows you to add some useful materials in your different objects.
+ * This module allows you to define and apply some useful materials on your different objects.
+ *
  *
  *
  * @{
  */
+
+/**
+ * Defines a sort of plastic material.
+ *
+ * Set a sort of plastic material, whose color is define by(@p r, @p g, @p b).
+ *
+ * @note This material act as a simple texture.
+ * @note There is no one reflection on this material.
+ *
+ */
+plastic(r:real, g:real, b:real);
+
+/**
+ * Defines a fresnel material.
+ *
+ * Create a material subject to fresnel reflection/refraction (for instance, @ref gold, @ref glass, etc.).
+ *
+ * @param r red component of the material color.
+ * @param g green component of the material color.
+ * @param b blue component of the material color.
+ * @param a alpha-channel, transparency of the color. 0 define a no color material and 1 an fully color one.
+ * @param n indice of refraction of the material.
+ * @param roughness roughness of the material. 0 define a transparent material and 1 an opaque one.
+ *
+ * @attention A @ref cube_map must be define previously in order to define reflection/refraction effects.
+ * @attention This material must be define just before the choosen object in order to be subject correctly to all transformations.
+ *
+ * @note This material act as a simple texture.
+ *
+ * @see Some indices of refraction can be found at http://www.pixelandpoly.com/ior.html
+ */
+fresnel_material(r:real, g:real, b:real, a:real, n:real, roughness:real);
+
+/**
+ * Defines a sort of gold material.
+ *
+ * Apply a sort of gold material to an object.
+ *
+ * @note This material is subject to fresnel reflection/refraction. It can be also define by
+  @code
+     fresnel_material 0.65, 0.607, 0.031, 0.7, 0.47, 1.0
+  @endcode
+ *
+ */
+gold();
+
+/**
+ * Defines a sort of silver material.
+ *
+ * Apply a sort of silver material to an object.
+ *
+ * @note This material is subject to fresnel reflection/refraction. It can be also define by
+ @code
+    fresnel_material 0.807, 0.807, 0.807, 0.4, 0.8, 1.0
+ @endcode
+ *
+ */
+silver();
+
+/**
+ * Defines a sort of bronze material.
+ *
+ * Apply a sort of bronze material to an object.
+ *
+ * @note This material is subject to fresnel reflection/refraction. It can be also define by
+ @code
+    fresnel_material 0.34, 0.24, 0.1, 0.93, 1.18, 1.0
+ @endcode
+ *
+ */
+bronze();
+
+/**
+ * Defines a sort of iron material.
+ *
+ * Apply a sort of iron material to an object.
+ *
+ * @note This material is subject to fresnel reflection/refraction. It can be also define by
+ @code
+    fresnel_material 0.43, 0.376, 0.364, 0.6, 2.95, 1.0
+ @endcode
+ *
+ */
+iron();
+
+/**
+ * Defines a sort of chromium material.
+ *
+ * Apply a sort of chromium material to an object.
+ *
+ * @note This material is subject to fresnel reflection/refraction. It can be also define by
+ @code
+    fresnel_material 0.8, 0.8, 0.8, 0.1, 2.97, 1.0
+ @endcode
+ *
+ */
+chromium();
+
+/**
+ * Defines a sort of emerald material.
+ *
+ * Apply a sort of emerald material to an object.
+ *
+ * @note This material is subject to fresnel reflection/refraction. It can be also define by
+ @code
+    fresnel_material 0.0, 0.6, 0.1, 0.65, 1.56, 0.03
+ @endcode
+ *
+ */
+emerald();
+
+/**
+ * Defines a sort of ruby material.
+ *
+ * Apply a sort of ruby material to an object.
+ *
+ * @note This material is subject to fresnel reflection/refraction. It can be also define by
+ @code
+    fresnel_material 0.6, 0.1, 0.1, 0.7, 1.757, 0.03
+ @endcode
+ *
+ */
+ruby();
+
+/**
+ * Defines a sort of diamond material.
+ *
+ * Apply a sort of diamond material to an object.
+ *
+ * @note This material is subject to fresnel reflection/refraction. It can be also define by
+ @code
+    fresnel_material 1.0, 1.0, 1.0, 0.1, 2.458, 0.03
+ @endcode
+ *
+ */
+diamond();
+
+/**
+ * Defines a sort of glass material.
+ *
+ * Apply a sort of glass material to an object.
+ *
+ * @note This material is subject to fresnel reflection/refraction. It can be also define by
+ @code
+    fresnel_material 1.0, 1.0, 1.0, 0.1, 1.12, 0.02
+ @endcode
+ *
+ */
+glass();
+
+/**
+ * Defines a sort of water material.
+ *
+ * Apply a sort of water material to an object.
+ *
+ * @note This material is subject to fresnel reflection/refraction. It can be also define by
+ @code
+    fresnel_material 0.35, 0.5, 0.5, 0.5, 1.325, 0.01
+ @endcode
+ *
+ */
+water();
+
 
 /**
 * @}
