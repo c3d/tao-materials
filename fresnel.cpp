3dfdd944937be70d1a87f8c9a8aef70a19470f80
Soulisse Baptiste <baptiste.soulisse@taodyne.com>
2011-12-09 16:11:46 +0100

    Add taodyne copyright to all glsl shaders. Closes #1719."

--- fresnel.cpp
+++ fresnel.cpp
@@ -43,6 +43,19 @@ Fresnel::Fresnel(uint unit, float IoR, float ratio, float roughness)
 
         // Basic vertex shader
         static string vSrc =
+                "/********************************************************************************\n"
+                "**                                                                               \n"
+                "** Copyright (C) 2011 Taodyne.                                                   \n"
+                "** All rights reserved.                                                          \n"
+                "** Contact: Taodyne (contact@taodyne.com)                                        \n"
+                "**                                                                               \n"
+                "** This file is part of the Tao Presentations application, developped by Taodyne.\n"
+                "** It can be only used in the software and these modules.                        \n"
+                "**                                                                               \n"
+                "** If you have questions regarding the use of this file, please contact          \n"
+                "** Taodyne at contact@taodyne.com.                                               \n"
+                "**                                                                               \n"
+                "********************************************************************************/\n"
                 "uniform vec3 camera;"
                 "uniform mat4 modelMatrix;"
 
@@ -89,7 +102,21 @@ Fresnel::Fresnel(uint unit, float IoR, float ratio, float roughness)
         {
             // If the extension is available, use this shader
             // to handle multiple lights
-            fSrc =  "#extension GL_EXT_gpu_shader4 : require\n"
+            fSrc =
+                    "/********************************************************************************\n"
+                    "**                                                                               \n"
+                    "** Copyright (C) 2011 Taodyne.                                                   \n"
+                    "** All rights reserved.                                                          \n"
+                    "** Contact: Taodyne (contact@taodyne.com)                                        \n"
+                    "**                                                                               \n"
+                    "** This file is part of the Tao Presentations application, developped by Taodyne.\n"
+                    "** It can be only used in the software and these modules.                        \n"
+                    "**                                                                               \n"
+                    "** If you have questions regarding the use of this file, please contact          \n"
+                    "** Taodyne at contact@taodyne.com.                                               \n"
+                    "**                                                                               \n"
+                    "********************************************************************************/\n"
+                    "#extension GL_EXT_gpu_shader4 : require\n"
 
                     "/* Material parameters */"
                     "uniform float IoR;" // Index of refraction
@@ -228,6 +255,19 @@ Fresnel::Fresnel(uint unit, float IoR, float ratio, float roughness)
             // If the extension is not available, use this shader
             // to handle an unique light.
             fSrc =
+                    "/********************************************************************************\n"
+                    "**                                                                               \n"
+                    "** Copyright (C) 2011 Taodyne.                                                   \n"
+                    "** All rights reserved.                                                          \n"
+                    "** Contact: Taodyne (contact@taodyne.com)                                        \n"
+                    "**                                                                               \n"
+                    "** This file is part of the Tao Presentations application, developped by Taodyne.\n"
+                    "** It can be only used in the software and these modules.                        \n"
+                    "**                                                                               \n"
+                    "** If you have questions regarding the use of this file, please contact          \n"
+                    "** Taodyne at contact@taodyne.com.                                               \n"
+                    "**                                                                               \n"
+                    "********************************************************************************/\n"
                     "/* Material parameters */"
                     "uniform float IoR;" // Index of refraction
                     "uniform float ratio;"
