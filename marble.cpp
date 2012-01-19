76f28dd5f5b49b0f21190b9180de45afbe8f89ff
Jerome Forissier <jerome@taodyne.com>
2012-01-19 17:58:45 +0100

    Allow normal use of unlicensed modules for 5 minutes. Closes #1858.

--- marble.cpp
+++ marble.cpp
@@ -108,7 +108,7 @@ void Marble::Draw()
         licensed = tao->checkLicense("Materials 1.0", false);
         tested = true;
     }
-    if (!licensed && !tao->blink(1.0, 0.2))
+    if (!licensed && !tao->blink(1.0, 0.2, 300.0))
         return;
 
     checkGLContext();
