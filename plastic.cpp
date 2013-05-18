19818530b7b37a61f6ebd4c2eb37032bc7f41917
Jerome Forissier <jerome@taodyne.com>
2013-05-18 15:08:44 +0200

    Merge branch 'master' of ssh://nano.taodyne.com/~ddd/Work/tao into merge

--- plastic.cpp
+++ plastic.cpp
@@ -114,7 +114,7 @@ void Plastic::Draw()
         // Get and set camera position
         Vector3 cam;
         tao->getCamera(&cam, NULL, NULL, NULL);
-        GLfloat camera[3] = { (float) cam.x, (float) cam.y, (float) cam.z};
+        GLfloat camera[3] = {cam.x, cam.y, cam.z};
         glUniform3fv(uniforms["camera"], 1, camera);
 
 
