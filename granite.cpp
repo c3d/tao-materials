346dcf2327a15474d488353822eef8d4ebaa72ba
Jerome Forissier <jerome@taodyne.com>
2013-10-16 11:56:53 +0200

    Update welcome page for Player. Closes #3091.

--- granite.cpp
+++ granite.cpp
@@ -140,7 +140,6 @@ void Granite::Draw()
         GL.Uniform3fv(uniforms["first_color"], 1, first_color);
         GL.Uniform3fv(uniforms["second_color"], 1, second_color);
         GL.Uniform3fv(uniforms["third_color"], 1, third_color);
-        GL.Uniform(uniforms["unit"], (GL.ActiveTextureUnitIndex() - GL_TEXTURE0));
 
         if(tao->isGLExtensionAvailable("GL_EXT_gpu_shader4"))
         {
@@ -184,7 +183,6 @@ void Granite::createShaders()
                 "varying vec3 viewDir;"
                 "varying vec3 normal;"
 
-                "uniform int unit;"
                 "uniform float scale;"
                 "void main()"
                 "{"
@@ -192,14 +190,7 @@ void Granite::createShaders()
                 "   gl_Position = ftransform();"
 
                 "   /* Compute texture coordinates */"
-                "   if(unit == 0)"
-                "       gl_TexCoord[0] = (scale * gl_TextureMatrix[0] * gl_MultiTexCoord0);"
-                "   else if(unit == 1)"
-                "       gl_TexCoord[0] = (scale * gl_TextureMatrix[1] * gl_MultiTexCoord1);"
-                "   else if(unit == 2)"
-                "       gl_TexCoord[0] = (scale * gl_TextureMatrix[2] * gl_MultiTexCoord2);"
-                "   else if(unit == 3)"
-                "       gl_TexCoord[0] = (scale * gl_TextureMatrix[3] * gl_MultiTexCoord3);"
+                "   gl_TexCoord[0] = (scale * gl_Vertex) / 100.0;"
 
                 "   /* Compute world position and normal */"
                 "   normal  = gl_NormalMatrix * gl_Normal;"
@@ -234,7 +225,6 @@ void Granite::createShaders()
                "uniform sampler3D noiseMap;"
 
                "uniform int       lights;"
-               "uniform int       unit;"
 
                "varying vec3 viewDir;"
                "varying vec3 normal;"
@@ -374,7 +364,6 @@ void Granite::createShaders()
                "uniform vec3      third_color;"
 
                "uniform sampler3D noiseMap;"
-               "uniform int       unit;"
 
                "varying vec3 viewDir;"
                "varying vec3 normal;"
@@ -477,7 +466,6 @@ void Granite::createShaders()
             // Save uniform locations
             uint id = pgm->programId();
 
-            uniforms["unit"] = glGetUniformLocation(id, "unit");
             uniforms["scale"] = glGetUniformLocation(id, "scale");
             uniforms["lights"] = glGetUniformLocation(id, "lights");
             uniforms["noiseMap"] = glGetUniformLocation(id, "noiseMap");
